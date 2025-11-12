139753b21c8bc9e2a95216f85c065f8afa2e5120
Wojti <64313768+Wojti9@users.noreply.github.com>
2025-11-12 20:00:09 +0100

    temp creating decks

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CategoryPage from "./components/CategoryPage.jsx";
 import DeckPage from "./components/DeckPage.jsx";
 import LoginRegisterPage from "./components/LoginRegisterPage.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import TempCreateDeck from "./components/TempCreateDeck.jsx";
 
 function Home() {
     return (
@@ -33,6 +34,7 @@ function App() {
                     <Route path="/register" element={<LoginRegisterPage />} />
                     <Route path="/:categoryId/decks" element={<CategoryPage />} />
                     <Route path="/:categoryId/:deckId" element={<DeckPage />} />
+                    <Route path="/temp_create" element={<TempCreateDeck />} />
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
